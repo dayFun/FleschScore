32d79e5fee7e26ee4e1540dfdfb07a2c32a26105
dayFun <dayFun@asynchrony.com>
2016-02-12 21:08:26 -0600

    Refactor tests.

--- test/document/BasicDocumentTest.java
+++ test/document/BasicDocumentTest.java
@@ -11,117 +11,64 @@ public class BasicDocumentTest {
     private BasicDocument basicDocument;
 
     @Test
-    public void testNumWords_13_Words_UCSD_Case_1() throws Exception {
+    public void testUCSD_Case_1() throws Exception {
         basicDocument = new BasicDocument("This is a test.  How many???  "
                 + "Senteeeeeeeeeences are here... there should be 5!  Right?");
 
         assertThat(basicDocument.getNumWords(), is(equalTo(13)));
+        assertThat(basicDocument.getNumSentences(), is(equalTo(5)));
     }
 
     @Test
-    public void testNumWords_0_Words_UCSD_Case_2() throws Exception {
+    public void testUCSD_Case_2() throws Exception {
         basicDocument = new BasicDocument("");
 
         assertThat(basicDocument.getNumWords(), is(equalTo(0)));
+        assertThat(basicDocument.getNumSentences(), is(equalTo(0)));
     }
 
     @Test
-    public void testNumWords_11_Words_UCSD_Case_3() throws Exception {
+    public void testUCSD_Case_3() throws Exception {
         basicDocument = new BasicDocument("sentence, with, lots, of, commas.!  "
                 + "(And some poaren)).  The output is: 7.5.");
 
         assertThat(basicDocument.getNumWords(), is(equalTo(11)));
+        assertThat(basicDocument.getNumSentences(), is(equalTo(4)));
     }
 
     @Test
-    public void testNumWords_3_Words_UCSD_Case_4() throws Exception {
+    public void testUCSD_Case_4() throws Exception {
         basicDocument = new BasicDocument("many???  Senteeeeeeeeeences are");
 
         assertThat(basicDocument.getNumWords(), is(equalTo(3)));
+        assertThat(basicDocument.getNumSentences(), is(equalTo(2)));
     }
 
     @Test
-    public void testNumWords_33_Words_UCSD_Case_5() throws Exception {
+    public void testUCSD_Case_5() throws Exception {
         basicDocument = new BasicDocument("Here is a series of test sentences. Your program should "
                 + "find 3 sentences, 33 words, and 49 syllables. Not every word will have "
                 + "the correct amount of syllables (example, for example), "
                 + "but most of them will.");
 
         assertThat(basicDocument.getNumWords(), is(equalTo(33)));
-    }
-
-    @Test
-    public void testNumWords_1_Word_UCSD_Case_6_7_8() throws Exception {
-        basicDocument = new BasicDocument("Segue");
-
-        assertThat(basicDocument.getNumWords(), is(equalTo(1)));
-    }
-
-    @Test
-    public void testNumWords_15_Words_UCSD_Case_9() throws Exception {
-        basicDocument = new BasicDocument("Lorem ipsum dolor sit amet, qui ex choro "
-                + "quodsi moderatius, nam dolores explicari forensibus ad.");
-
-        assertThat(basicDocument.getNumWords(), is(equalTo(15)));
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////
-
-    @Test
-    public void testNumSentences_5_Sentences_UCSD_Case_1() throws Exception {
-        basicDocument = new BasicDocument("This is a test.  How many???  "
-                + "Senteeeeeeeeeences are here... there should be 5!  Right?");
-
-        assertThat(basicDocument.getNumSentences(), is(equalTo(5)));
-    }
-
-    @Test
-    public void testNumSentences_0_Sentences_UCSD_Case_2() throws Exception {
-        basicDocument = new BasicDocument("");
-
-        assertThat(basicDocument.getNumSentences(), is(equalTo(0)));
-    }
-
-    @Test
-    public void testNumSentences_4_Sentences_UCSD_Case_3() throws Exception {
-        basicDocument = new BasicDocument("sentence, with, lots, of, commas.!  "
-                + "(And some poaren)).  The output is: 7.5.");
-
-        assertThat(basicDocument.getNumSentences(), is(equalTo(4)));
-    }
-
-    @Test
-    public void testNumSentences_2_Sentences_UCSD_Case_4() throws Exception {
-        basicDocument = new BasicDocument("many???  Senteeeeeeeeeences are");
-
-        assertThat(basicDocument.getNumSentences(), is(equalTo(2)));
-    }
-
-    @Test
-    public void testNumSentences_3_Sentences_UCSD_Case_5() throws Exception {
-        basicDocument = new BasicDocument("Here is a series of test sentences. Your program should "
-                + "find 3 sentences, 33 words, and 49 syllables. Not every word will have "
-                + "the correct amount of syllables (example, for example), "
-                + "but most of them will.");
-
         assertThat(basicDocument.getNumSentences(), is(equalTo(3)));
     }
 
     @Test
-    public void testNumSentences_1_Sentence_UCSD_Case_6_7_8() throws Exception {
+    public void testUCSD_Case_6_7_8() throws Exception {
         basicDocument = new BasicDocument("Segue");
 
+        assertThat(basicDocument.getNumWords(), is(equalTo(1)));
         assertThat(basicDocument.getNumSentences(), is(equalTo(1)));
     }
 
     @Test
-    public void testNumSentences_15_Sentences_UCSD_Case_9() throws Exception {
+    public void testUCSD_Case_9() throws Exception {
         basicDocument = new BasicDocument("Lorem ipsum dolor sit amet, qui ex choro "
                 + "quodsi moderatius, nam dolores explicari forensibus ad.");
 
+        assertThat(basicDocument.getNumWords(), is(equalTo(15)));
         assertThat(basicDocument.getNumSentences(), is(equalTo(1)));
     }
-
-    //////////////////////////////////////////////////////////////////////////////////////////////////////
-
 }
